e58c17ba7500c1b713d9c5d693eaec7ffb98378a
VitaL <vitaldukm@gmail.com>
2021-12-16 19:01:44 +0000

    DAY 35 - Rain Notifier SMS Application

--- rain-alert-main/main.py
+++ rain-alert-main/main.py
@@ -1,9 +1,21 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+import config
 
-my_lat = 52.486244
-my_lon = -1.890401
-api_key = "9f83d4d2dda67c80b05482e5cc252eb2"
+my_lat = config.my_lat
+my_lon = config.my_lon
+api_key = config.OWM_api_key
 exclusions = "current,minutely,daily"
+
+#Twilio
+auth_token = config.twilio_auth_token
+account_sid = config.twilio_account_sid
+twilio_phone_number = config.twilio_phone_num
+my_phone_number = config.my_phone_num
+
+
 
 parameters = {
     "lat": my_lat,
@@ -24,6 +36,21 @@ list_of_id = [i["weather"][0]["id"] for i in weather_data["hourly"][:12]]
 
 print(list_of_id)
 
+will_rain = False
+
 for i in list_of_id:
     if i < 700:
-        print("Bring an umbrella")
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an Umbrella",
+        from_=twilio_phone_number,
+        to=my_phone_number
+    )
+    print(message.status)
