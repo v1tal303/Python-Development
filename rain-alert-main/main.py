b62c2aca1381077481a652bca9a61e38259f9444
VitaL <vitaldukm@gmail.com>
2021-12-16 17:58:30 +0000

    IDE Test

--- rain-alert-main/main.py
+++ rain-alert-main/main.py
@@ -1,1 +1,17 @@
+import requests
+
+my_lat = 52.486244
+my_lon = -1.890401
 api_key = "9f83d4d2dda67c80b05482e5cc252eb2"
+exclusions = "hourly,daily"
+
+parameters = {
+    "lat": my_lat,
+    "lon": my_lon,
+    "exclude": exclusions,
+    "appid": api_key,
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
