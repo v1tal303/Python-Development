d58ff9b36785c12772b65da0d816dac9a79f8f6f
VitaL <vitaldukm@gmail.com>
2021-12-16 18:15:43 +0000

    IDE Test

--- rain-alert-main/main.py
+++ rain-alert-main/main.py
@@ -3,7 +3,7 @@ import requests
 my_lat = 52.486244
 my_lon = -1.890401
 api_key = "9f83d4d2dda67c80b05482e5cc252eb2"
-exclusions = "hourly,daily"
+exclusions = "current,minutely,daily"
 
 parameters = {
     "lat": my_lat,
@@ -15,3 +15,15 @@ parameters = {
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
 weather_data = response.json()
+
+# list_of_id = []
+# for i in weather_data["hourly"][:12]:
+#     list_of_id.append(i["weather"][0]["id"])
+
+list_of_id = [i["weather"][0]["id"] for i in weather_data["hourly"][:12]]
+
+print(list_of_id)
+
+for i in list_of_id:
+    if i < 700:
+        print("Bring an umbrella")
