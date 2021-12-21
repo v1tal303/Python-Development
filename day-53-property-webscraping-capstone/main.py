c373a1c79ce3b7ed598912f8406fdc0a68c76a21
VitaL <vitaldukm@gmail.com>
2021-12-21 17:47:29 +0000

    Day 53 - Zillow Residential Properties Webscrape

--- day-53-property-webscraping-capstone/main.py
+++ day-53-property-webscraping-capstone/main.py
@@ -15,7 +15,6 @@ def get_properties():
     response = requests.get(
         "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.69219435644531%2C%22east%22%3A-122.17446364355469%2C%22south%22%3A37.703343724016136%2C%22north%22%3A37.847169233586946%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D",
         headers=headers)
-    SF_data = response.text
     soup = BeautifulSoup(response.text, "html.parser")
 
     list_of_links = soup.find_all(class_="list-card-link list-card-link-top-margin", href=True)
@@ -25,8 +24,6 @@ def get_properties():
     prices = [i.getText() for i in list_of_prices]
     address = [i.getText() for i in list_of_address]
     links = ["www.test.com" for i in list_of_prices]
-    property_dict = {}
-
     return [address, prices, links]
 
 # Submit the information into google forms which can be viewed in an excel spreadsheet
